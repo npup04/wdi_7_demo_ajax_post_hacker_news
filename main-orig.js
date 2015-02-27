0275df8c045dc03ee111de42c711930589600d29
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-02-27 00:15:54 -0500

    Ajax post working, displays new users at top of list in descending order

---
+++ main-orig.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $.ajax({
+    url: 'http://ig-hacker-news.herokuapp.com/users',
+    // this url comes from rake routes, make sure rake routes says this url accepts a POST request
+    // also make sure that the controller action for this url exists and saves to the database correctly
+    type: 'POST',
+    // for a GET request you don't have to specify the type of request, because AJAX defaults to GET
+    // for a POST request you have to specify the type of request manually
+    data: {user: {name: 'bobface', about: 'human', email: 'bob@bob.com'}},
+    // the data object contains all the info you are sending to the Rails API
+    // the attributes have to match your database schema
+    // by default, you need to specify the model manually - in this case, user
+    // so {user: {name, about, email} } instead of just {name, about, email}
+    // but if you are using a serializer, this behavior may be different depending on how you set up the serializer
+    // make sure that in your controller, you have params set to allow this info (aka 'strong params')
+    // usually this is a private method with params.require(:user).permit(:name, :about, :email)
+    dataType: 'JSON'
+    // dataType here refers to what is being RETURNED from the POST request, not what's being sent
+  })
+  .done(function(results) {
+    console.log("here is the user I just created, click on the triangle to expand");
+    console.table(results);
+    // do other stuff with the user you just created here
+    // to do that, generally you would call functions that are defined elsewhere in your Javascript file
+    // and either pass in 'results' as the argument to those functions
+    // or assign 'results' to a property of an existing Javascript object
+  });
+
+  // the same POST request as above, but without all the comments
+  $.ajax({
+    url: 'http://ig-hacker-news.herokuapp.com/users',
+    type: 'POST',
+    data: {user: {name: 'steve jobs', about: 'god or anti-god?', email: 'steve@apple.com'}},
+    dataType: 'JSON'
+  })
+  .done(function(results) {
+    console.log("here is another user I just created, click on the triangle to expand");
+    console.table(results);
+  });
+
+  $.ajax({
+    url: 'http://ig-hacker-news.herokuapp.com/users',
+    type: 'GET',
+  })
+  .done(function(results) {
+    console.log("here are all the users in my database")
+    console.table(results);
+  });
+});
--- main.js
+++ main.js
@@ -1,49 +1,79 @@
+//Assignment:
+// modify this file so that on index.html,
+// a visitor can:
+//  - see a list of all the users,
+//  - fill out a form to create a new user, hit submit,
+// and see the new user added to the list of users
+
+var HackerApp = HackerApp || {};
+
+HackerApp.create_a_user = function(event){
+  event.preventDefault(); //prevent the default behavior of submit button
+    $.ajax({
+    url: 'http://ig-hacker-news.herokuapp.com/users',
+    type: 'POST',
+    dataType: 'JSON',
+    data:
+      {user: {
+        name: $('#name').val(),
+        about: $('#about').val(),
+        email: $('#email').val()
+        }
+      }
+  })
+  .done(function(results) {
+    // console.table(results);
+    // HackerApp.show_user(results); //works w/ users in ascending order
+    // HackerApp.get_all_users(results); //doesn't work, isn't showing newly added user in list
+    HackerApp.prepend_user(results);  //works, prepend the new user to the top of the descending sorted list
+    console.log("New user added!!!");
+  });
+
+}; //end HackerApp.create_a_user
+
+HackerApp.get_all_users = function(users){
+  //sorter function, sort users in descending order
+  function _sorter(a,b) {
+    return (b.id) - (a.id);
+  };
+  var sorted_users = users.sort(_sorter);
+
+  //iterate users
+  for(var i = 0; i < sorted_users.length; i++){
+    HackerApp.show_user(sorted_users[i]);
+  };//end for
+};//end HackerApp.get_all_users
+
+HackerApp.show_user = function(user){
+  $('#all_users').append(user.id + ' | ');
+  $('#all_users').append(user.name + ' | ');
+  $('#all_users').append(user.about + ' | ');
+  $('#all_users').append(user.email + '<br>');
+};
+
+//display newly added user at top of list
+HackerApp.prepend_user = function(user){
+  $('#all_users').prepend(' <br> ');
+  $('#all_users').prepend(user.email);
+  $('#all_users').prepend(user.about + ' | ');
+  $('#all_users').prepend(user.name + ' | ');
+  $('#all_users').prepend(user.id + ' | ');
+};
+
 $(document).ready(function() {
   $.ajax({
     url: 'http://ig-hacker-news.herokuapp.com/users',
-    // this url comes from rake routes, make sure rake routes says this url accepts a POST request
-    // also make sure that the controller action for this url exists and saves to the database correctly
-    type: 'POST',
-    // for a GET request you don't have to specify the type of request, because AJAX defaults to GET
-    // for a POST request you have to specify the type of request manually
-    data: {user: {name: 'bobface', about: 'human', email: 'bob@bob.com'}}, 
-    // the data object contains all the info you are sending to the Rails API
-    // the attributes have to match your database schema
-    // by default, you need to specify the model manually - in this case, user
-    // so {user: {name, about, email} } instead of just {name, about, email}
-    // but if you are using a serializer, this behavior may be different depending on how you set up the serializer
-    // make sure that in your controller, you have params set to allow this info (aka 'strong params')
-    // usually this is a private method with params.require(:user).permit(:name, :about, :email)
-    dataType: 'JSON' 
-    // dataType here refers to what is being RETURNED from the POST request, not what's being sent
+    type: 'GET'
   })
   .done(function(results) {
-    console.log("here is the user I just created, click on the triangle to expand");
-    console.table(results);
-    // do other stuff with the user you just created here
-    // to do that, generally you would call functions that are defined elsewhere in your Javascript file
-    // and either pass in 'results' as the argument to those functions
-    // or assign 'results' to a property of an existing Javascript object
-  });
-  
-  // the same POST request as above, but without all the comments
-  $.ajax({
-    url: 'http://ig-hacker-news.herokuapp.com/users',
-    type: 'POST',
-    data: {user: {name: 'steve jobs', about: 'god or anti-god?', email: 'steve@apple.com'}}, 
-    dataType: 'JSON' 
-  })
-  .done(function(results) {
-    console.log("here is another user I just created, click on the triangle to expand");
-    console.table(results);
+    // console.log("here are all the users in my database")
+    // console.table(results);
+    console.log("GET success");
+    console.log(results);
+    HackerApp.get_all_users(results);
+
   });
 
-  $.ajax({
-    url: 'http://ig-hacker-news.herokuapp.com/users',
-    type: 'GET',
-  })
-  .done(function(results) {
-    console.log("here are all the users in my database")
-    console.table(results);
-  });
+  $('#submit').on('click', HackerApp.create_a_user);
+
 });
